7cab7242e12420073775986cf2a2a30e11161a99
Sharone Small <sharonemichal@gmail.com>
2014-02-23 20:18:58 -0500

    Leaflet!

--- Wireframe1.js
+++ Wireframe1.js
@@ -2,9 +2,14 @@ $(document).ready(function(){
     $('#header').click(function(){
     	$(this).fadeTo('fast',0);
     });
-	var map = L.map("map")
-	map.setView([51.505, -0.09], 18);
-	L.tileLayer('http://{s}.tile.cloudmade.com/API-key/997/256/{z}/{x}/{y}.png', {
-    	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	var map = L.map("map");
+	map.setView([42.359200, -71.091950], 16);
+	L.tileLayer('http://tile.cloudmade.com/440e7bdbfe0444b18cca210e9cb056c5/997/256/{z}/{x}/{y}.png', {
+			attribution: 'Map data &copy CloudMade',
+	}).addTo(map);
+	var circle = L.circle([42.359200,-71.091950], 10, {
+    	color: 'red',
+    	fillColor: '#f03',
+    	fillOpacity: 0.5
 	}).addTo(map);
 });
