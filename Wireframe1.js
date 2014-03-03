118e10c4229d5925f4ce2488fe6775b243488d2c
Catherine Cheng <chengcatherine91@gmail.com>
2014-03-02 21:03:34 -0500

    cc changes
    can’t remember what changes were made

--- Wireframe1.js
+++ Wireframe1.js
@@ -1,3 +1,4 @@
+
 function sensor(lat,lon,location) {
 	this.lat = lat;
 	this.lon = lon;
@@ -18,7 +19,11 @@ var nodeLong = [-71.091843, -71.093201];
 	map.setView([42.359200, -71.091950], 16);
 	L.tileLayer('http://tile.cloudmade.com/440e7bdbfe0444b18cca210e9cb056c5/997/256/{z}/{x}/{y}.png', { attribution: 'Map data &copy CloudMade',
 	}).addTo(map);
+<<<<<<< HEAD
 
+=======
+	
+>>>>>>> 76bdea0a8e6b6fe031088a4545dd40144165ee3f
 	//Nodes
 	for(var i=0; i<sensors.length; i++){
 		sensors[i].circ = L.circle([sensors[i].lat,sensors[i].lon], 16, {
