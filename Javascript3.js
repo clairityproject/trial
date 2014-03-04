5f754c15862ecf2048070d7019906e1a377af25a
Sharone Small <sharonemichal@gmail.com>
2014-03-03 20:53:28 -0500

    Nodes Side Display

--- Javascript3.js
+++ Javascript3.js
@@ -1,6 +1,9 @@
-//Question: How do I get them to appear when the site loads?
+//Question: How do I get them to appear when the site loads? - split into multiple threads
+//Redrawing Circles
 
-var serverURL = "http://ec2-54-201-87-182.us-west-2.compute.amazonaws.com/api/v1/node/";
+var serverNodesURL = "http://ec2-54-201-87-182.us-west-2.compute.amazonaws.com/api/v1/node/";
+var serverDataURL = "http://ec2-54-201-87-182.us-west-2.compute.amazonaws.com/api/v1/datapoint/";
+
 function sensor(lat,lon,location) {
 	this.lat = lat;
 	this.lon = lon;
@@ -9,19 +12,36 @@ function sensor(lat,lon,location) {
 
 var sensors = [];
 var new_sensor;
+var nodesDrawn = false;
 
-function Request() {
-	$.getJSON(serverURL, function (data) {
+function RequestNodes() {
+	$.getJSON(serverNodesURL, function (data) {
 		for(i=0; i<data["objects"].length; i++){
 			new_sensor = new sensor(data["objects"][i]["location"]["latitude"],data["objects"][i]["location"]["longitude"],data["objects"][i]["location"]["name"]);
     		sensors.push(new_sensor);
+    		nodesDrawn = true;
 		}
 	});
 }
 
+function RequestDatapoints() {
+	if(nodesDrawn){
+		$.getJSON(serverDataURL, function (data) {
+			for(i=0; i<data["objects"].length; i++){
+				alpha1 = data["objects"][i]["alphasense_1"];
+				sensors[i].alpha1 = alpha1;
+			}
+		});
+	}
+}
+
+function displayHover(i){
+	$("#locationheader").html(String(sensors[i].alpha1));
+};
+
 $(document).ready(function(){
 
-	Request();
+	RequestNodes();
 
     //Leaflet Map
     var sWBound = L.latLng(42.365901,-71.079440);
@@ -33,28 +53,31 @@ $(document).ready(function(){
 
 	//Nodes
 	function drawNodes(){
+		RequestDatapoints();
 		for(var i=0; i<sensors.length; i++){
 			sensors[i].circ = L.circle([sensors[i].lat,sensors[i].lon], 16, {
 	    		color: 'red',
 	    		fillColor: '#f03',
 	    		fillOpacity: 0.5
 			}).addTo(map);
+
+			sensors[i].circ.number = i;
 	
 			sensors[i].circ.bindPopup(sensors[i].location, {closeButton: false});
-			function locationHover(evt){
+			sensors[i].circ.on('mouseover', function(evt) {
 				evt.target.openPopup();
-			};
-			sensors[i].circ.on('mouseover', locationHover);
+				displayHover(this.number);
+			});
 			sensors[i].circ.on('mouseout', function(evt){
 				evt.target.closePopup();
 			});
-	
+
 		};
 	}
 
 	drawNodes();
 
-	var reset = setInterval(function() {drawNodes()}, 15000);
+	var reset = setInterval(function() {drawNodes()}, 5000);
 
 
 });
