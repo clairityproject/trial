d345ab9fdfd1d984f7a0dcff283d67b1878f11c2
Catherine Cheng <chengcatherine91@gmail.com>
2014-03-05 10:24:01 -0500

    catherine's updates

--- Wireframe1.js
+++ Wireframe1.js
@@ -4,8 +4,9 @@ function sensor(lat,lon,location) {
 	this.lon = lon;
 	this.location = location;
 }
-$(document).ready(function(){
 
+
+$(document).ready(function(){
 var sensors = [];
 sensors.push(new sensor(42.359765,-71.091843,"Lobby 10"));
 sensors.push(new sensor(42.359133,-71.093201,"Lobby 7"));
@@ -19,11 +20,7 @@ var nodeLong = [-71.091843, -71.093201];
 	map.setView([42.359200, -71.091950], 16);
 	L.tileLayer('http://tile.cloudmade.com/440e7bdbfe0444b18cca210e9cb056c5/997/256/{z}/{x}/{y}.png', { attribution: 'Map data &copy CloudMade',
 	}).addTo(map);
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 76bdea0a8e6b6fe031088a4545dd40144165ee3f
 	//Nodes
 	for(var i=0; i<sensors.length; i++){
 		sensors[i].circ = L.circle([sensors[i].lat,sensors[i].lon], 16, {
@@ -44,3 +41,11 @@ var nodeLong = [-71.091843, -71.093201];
 	};
 
 });
+
+
+
+function($){
+        $(window).load(function(){
+            $("a[rel='m_PageScroll2id']").mPageScroll2id();
+        });
+    })(jQuery);
